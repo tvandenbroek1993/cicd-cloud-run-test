edb5feecba79209d58fb773d09ea4c88f73e6796
tvandenbroek1993 <tom.vandenbroek@springbokagency.com>
2023-10-04 16:06:28 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,44 @@
+import tensorflow as tf
+from flask import Flask, request, jsonify
+from tensorflow import keras
+from PIL import Image
+import numpy as np
+import io
+
+model = keras.models.load_model("mnist_model.h5")
+
+def transform_image(pillow_image):
+    data = np.asarray(pillow_image)
+    data = data / 255.0
+    data = data[np.newaxis, ..., np.newaxis]
+    # --> [1, x, y, 1]
+    data = tf.image.resize(data, [28, 28])
+    return data
+
+def predict(x):
+    predictions = model(x)
+    predictions = tf.nn.softmax(predictions)
+    pred0 = predictions[0]
+    label0 = np.argmax(pred0)
+    return label0
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        file = request.files.get('file')
+        if file is None or file.filename == "":
+            return jsonify({"error": "no file"})
+
+        try:
+            image_bytes = file.read()
+            pillow_img = Image.open(io.BytesIO(image_bytes)).convert('L')
+            tensor = transform_image(pillow_img)
+            prediction = predict(tensor)
+            data = {"prediction": int(prediction)}
+            return jsonify(data)
+        except Exception as e:
+            return jsonify({"error": str(e)})
+
+    return "OK"
